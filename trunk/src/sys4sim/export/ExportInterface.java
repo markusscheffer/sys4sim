cf39bbcab41748d260b77b572a2d2aa91011799f
lists@einbecker.net <lists@einbecker.net@250abc3f-f350-0410-9ca9-f7dda66bccbe>
2008-07-02 13:09:41 +0000

    initial commit.

---
+++ trunk/src/sys4sim/export/ExportInterface.java
@@ -0,0 +1,13 @@
+package sys4sim.export;
+
+import java.io.File;
+
+import sys4sim.internal_model.Model;
+
+public interface ExportInterface {
+	public void writeFile(Model model, File file);
+}
+
+
+https://sys4sim.googlecode.com/svn/trunk/
+	cm2wg2Tq2Sj2
